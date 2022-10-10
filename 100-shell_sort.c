19e2ac3b07ac5b1aadd01e1e9e8b46b3f3060a99
LordKcode <101455308+LordKcode@users.noreply.github.com>
2022-10-10 16:00:47 -0700

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,40 +1,61 @@
 #include "sort.h"
-/**
- * shell_sort -  sorts an array of integers in
- * ascending order using the Shell sort algorithm,
- * using the Knuth sequence
- * @array: pointer to array
- * @size: size of the array
- **/
-void shell_sort(int *array, size_t size)
-{
-	size_t kunth[1000], k = 0, j = 0, i;
-	int n, j2;
 
-	if (!array)
-		return;
-	while (j * 3 + 1 < size)
+/**
+ * selection_shell_sort - sorts the sub-array of integers in ascending order
+ * using the Selection algorithm
+ * @array: the array to be sorted
+ * @size: number of elements in @array
+ * @gap: interval that subdivide the array
+ * @index: initial position to sort
+ */
+void selection_shell_sort(int *array, size_t size, size_t gap, size_t index)
+{
+	size_t i, idx, min_idx;
+	int tmp, swapped = 0;
+
+	for (i = index; i < size; i = i + gap)
 	{
-		kunth[k] = j * 3 + 1;
-		j = kunth[k++];
-	}
-	for (i = 0; i < k; i++)
-	{
-		for (j = 0; j < size; j++)
+		swapped = 0;
+		min_idx = i;
+		for (idx = i + gap; idx < size; idx = idx + gap)
 		{
-			if ((j + kunth[k - i - 1]) > size - 1)
-				break;
-			j2 = j;
-			while (array[j2] > array[j2 + kunth[k - i - 1]])
+			if (array[idx] < array[min_idx])
 			{
-				n = array[j2];
-				array[j2] =  array[j2 + kunth[k - i - 1]];
-				array[j2 + kunth[k - i - 1]] = n;
-				j2 = j2 - kunth[k - i - 1];
-				if (j2 < 0)
-					break;
+				min_idx = idx;
+				swapped = 1;
 			}
 		}
+		if (swapped)
+		{
+			tmp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
+		}
+	}
+}
+
+/**
+ * shell_sort - sorts an array of integers in ascending order
+ * using the Shell sort algorithm and the Knuth sequence
+ * @array: the array to be sorted
+ * @size: number of elements in @array
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t i, gap = 1;
+
+	if (!array || size < 2)
+		return;
+
+	while (gap < size)
+		gap = (gap * 3) + 1;
+	gap = (gap - 1) / 3;
+
+	while (gap > 0)
+	{
+		for (i = 0; i < size; i++)
+			selection_shell_sort(array, size, gap, i);
+		gap = (gap - 1) / 3;
 		print_array(array, size);
 	}
 }
