364336c91127aff926ba9dfde111b201dd2157b8
LordKcode <101455308+LordKcode@users.noreply.github.com>
2022-10-11 15:18:40 -0700

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,34 +1,43 @@
 #include "sort.h"
 
 /**
- * shell_sort - sorts an array of integers in ascending order using the
- * Shell sort algorithm
- * @array: array to sort
- * @size: size of the array to sort
- *
- * Return: void
+ * shell_sort - Shell sort algorithm
+ * @array: unsorted data
+ * @size: large of array
+ * Return: Nothing
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t i, j, gap = 1;
-	int insert;
+	int tmp;
+	size_t j, gap, n;
 
-	if (array == NULL || size < 2)
+	gap = 1;
+	if (!array || size < 2)
 		return;
-
+	/* Create Knuth sequence */
 	while (gap < size / 3)
 		gap = gap * 3 + 1;
-
+	/* Start with the largest gap and work down to a gap of 1 */
 	while (gap > 0)
 	{
-		for (i = gap; i < size; i++)
+		/* Do a gapped insertion sort for this gap size. */
+		/* The first gap elements array[0..gap-1] are already in gapped order */
+		/* keep adding one more element until the entire array is gap sorted */
+		for (j = gap; j < size; j += 1)
 		{
-			insert = array[i];
-			for (j = i; j >= gap && array[j - gap] > insert; j = j - gap)
-				array[j] = array[j - gap];
-			array[j] = insert;
+			/* add array[j] to the elements that have been gap sorted */
+			/* save array[j] in temp and make a hole at position j */
+			tmp = array[j];
+			/* shift earlier gap-sorted elements up until the correct */
+			/* location for array[j] is found */
+			for (n = j; n >= gap && tmp < array[n - gap]; n -= gap)
+				array[n] = array[n - gap];
+			/* put tmp (the original array[j]) in its correct location */
+			array[n] = tmp;
 		}
-		gap = (gap - 1) / 3;
+		/* decreasing the interval */
+		gap /= 3;
+		/*  print the array each time you decrease the interval */
 		print_array(array, size);
 	}
 }
