9405ec6885e25c04c259ef55cd1a7100214e039e
LordKcode <101455308+LordKcode@users.noreply.github.com>
2022-10-15 14:21:01 -0700

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,43 +1,108 @@
 #include "sort.h"
 
+
 /**
- * shell_sort - Shell sort algorithm
- * @array: unsorted data
- * @size: large of array
- * Return: Nothing
+ * free_gap - Freeing the knuth gap sequence
+ * linked list
+ * @head: The pointer to the head of the linked
+ * list
  */
+
+
+void free_gap(k_gap_t *head)
+{
+	k_gap_t *ptr;
+
+	ptr = head;
+	while (head)
+	{
+		head = ptr->next;
+		free(ptr);
+		ptr = head;
+	}
+}
+/**
+ * knuth_gap - Implementing knuth gap sequence
+ * @size: The size of the array
+ * Return: an array containing the sequence based on
+ * the size of the array
+ */
+
+k_gap_t *knuth_gap(size_t size)
+{
+	k_gap_t *ptr, *new;
+	size_t seq, i = 1, j, pow;
+
+	seq = 1;
+	ptr = NULL;
+	while (seq <= size)
+	{
+		new = malloc(sizeof(k_gap_t));
+		if (!new)
+			return (NULL);
+		new->n = seq;
+		if (ptr)
+			ptr->next = new;
+		new->prev = ptr;
+		new->next = NULL;
+		ptr = new;
+		i++;
+		j = 0;
+		pow = 1;
+		while (j++ < i)
+			pow *= 3;
+		seq = (pow - 1) / 2;
+	}
+	return (ptr);
+}
+
+/**
+ * shell_sort - Using shell sort sorting algorithm
+ * to sort the array
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+
 void shell_sort(int *array, size_t size)
 {
-	int tmp;
-	size_t j, gap, n;
+	k_gap_t *ptr, *prev_ptr;
+	size_t i, j, cur;
+	int prev, pl;
 
-	gap = 1;
-	if (!array || size < 2)
+	if (array == NULL || size < 2)
 		return;
-	/* Create Knuth sequence */
-	while (gap < size / 3)
-		gap = gap * 3 + 1;
-	/* Start with the largest gap and work down to a gap of 1 */
-	while (gap > 0)
+	ptr = knuth_gap(size);
+	while (ptr)
 	{
-		/* Do a gapped insertion sort for this gap size. */
-		/* The first gap elements array[0..gap-1] are already in gapped order */
-		/* keep adding one more element until the entire array is gap sorted */
-		for (j = gap; j < size; j += 1)
+		i = 0;
+		while (i < ptr->n)
 		{
-			/* add array[j] to the elements that have been gap sorted */
-			/* save array[j] in temp and make a hole at position j */
-			tmp = array[j];
-			/* shift earlier gap-sorted elements up until the correct */
-			/* location for array[j] is found */
-			for (n = j; n >= gap && tmp < array[n - gap]; n -= gap)
-				array[n] = array[n - gap];
-			/* put tmp (the original array[j]) in its correct location */
-			array[n] = tmp;
+			j = i++;
+			while (j < size)
+			{
+				cur = j;
+				prev = j - ptr->n;
+				while (prev >= 0)
+				{
+					if (array[cur] < array[prev])
+					{
+						pl = array[cur];
+						array[cur] = array[prev];
+						array[prev] = pl;
+					}
+					else
+					{
+						break;
+					}
+					cur = prev;
+					prev = prev - ptr->n;
+				}
+				j += ptr->n;
+			}
 		}
-		/* decreasing the interval */
-		gap /= 3;
-		/*  print the array each time you decrease the interval */
+		prev_ptr = ptr;
+		ptr = ptr->prev;
 		print_array(array, size);
 	}
+	free_gap(prev_ptr);
 }
